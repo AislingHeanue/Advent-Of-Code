502f54c6ff78870d44bce8a187503aa1dbb6d40e
AislingHeanue <aislingheanue@gmail.com>
2023-12-03 12:06:31 +0000

    Refactor 3b Used unordered matrix to store data more efficiently.

--- 2023/core/day3/b.go
+++ 2023/core/day3/b.go
@@ -19,53 +19,64 @@ func bCommand() *cobra.Command {
 	}
 }
 
+type Coordinate struct {
+	y int
+	x int
+}
+
+type Element struct {
+	number               int
+	symbol               string
+	markedBySymbolCoords *map[Coordinate]bool
+}
+
 func partB(challenge *core.Input) int {
 	mat := challenge.TileMap()
-	markedMat := util.Map[rune, int](mat, func(y, x int, v rune) int {
-		return 0
-	})
-	numberMat := util.Map[rune, int](mat, func(y, x int, v rune) int {
+	matrix := util.MapToUnordered[rune, Element](mat, func(y, x int, v rune) Element {
 		if unicode.IsDigit(v) {
-			return int(v - '0')
+			newMap := make(map[Coordinate]bool)
+			return Element{
+				number:               int(v - '0'),
+				symbol:               ".",
+				markedBySymbolCoords: &newMap,
+			}
+		} else {
+			return Element{
+				number:               -1,
+				symbol:               string(v),
+				markedBySymbolCoords: nil,
+			}
 		}
-		return -1
-	})
-	symbolMat := util.Map[rune, string](mat, func(y, x int, v rune) string {
-		if unicode.IsDigit(v) {
-			return "."
-		}
-		return string(v)
 	})
 	total := 0
 
-	for y := 0; y < numberMat.GetHeight(); y++ {
-		for x := 0; x < numberMat.GetWidth(); x++ {
-			if symbol, marked := symbolMat.MustGet(y, x), markedMat.MustGet(y, x); symbol != "." && marked == 0 {
-				markedMat := util.Map[rune, int](mat, func(y, x int, v rune) int {
-					return 0
-				})
+	for y := 0; y < matrix.GetHeight(); y++ {
+		for x := 0; x < mat.GetWidth(); x++ {
+			if elem := matrix.MustGet(y, x); elem.symbol != "." {
 				values := []int{}
 				// check for numbers in surrounding area
 				for y2 := y - 1; y2 <= y+1; y2++ {
 					for x2 := x - 1; x2 <= x+1; x2++ {
-						number, ok := numberMat.Get(y2, x2)
-						if marked, alsoOk := markedMat.Get(y2, x2); number != -1 && ok && marked == 0 && alsoOk { // this line is terrible
+						newElem, ok := matrix.Get(y2, x2)
+						if ok && newElem.number != -1 && !(*newElem.markedBySymbolCoords)[Coordinate{y: y, x: x}] {
 							left := x2
 							right := x2
 							value := 0
 							// grow number to the left
-							for left > 0 && numberMat.MustGet(y2, left-1) != -1 {
+							for left > 0 && matrix.MustGet(y2, left-1).number != -1 {
 								left--
 							}
 							// grow number to the right
-							for right < numberMat.GetWidth()-1 && numberMat.MustGet(y2, right+1) != -1 {
+							for right < matrix.GetWidth()-1 && matrix.MustGet(y2, right+1).number != -1 {
 								right++
 							}
 							// scan the number and mark it as seen
 							for x3 := left; x3 <= right; x3++ {
+								elem3 := matrix.MustGet(y2, x3)
 								value *= 10
-								value += numberMat.MustGet(y2, x3)
-								markedMat.MustSet(y2, x3, 1)
+								value += elem3.number
+								(*elem3.markedBySymbolCoords)[Coordinate{y: y, x: x}] = true
+								matrix.MustSet(y2, x3, elem3)
 							}
 							values = append(values, value)
 						}
