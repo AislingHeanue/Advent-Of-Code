981e2016a14ebd889925d249ef6bb2e08fc7a4be
AislingHeanue <aislingheanue@gmail.com>
2026-02-22 16:33:55 +0000

    Install prettier

---
+++ 2025/.prettierrc.ts
@@ -0,0 +1,8 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  trailingComma: "none",
+  arrowParens: "avoid"
+};
+
+export default config;
--- 2025/src/days/01.ts
+++ 2025/src/days/01.ts
@@ -1,23 +1,28 @@
-import type { Input } from "../input"
+import type { Input } from "../input";
 
 export const solve = (input: Input, part: Part): number | undefined => {
-  if (part === 1) {
-    return undefined
-  } else {
-    return undefined
-  }
-}
+  return undefined;
+};
 
 export const solution: DaySolution = {
   solve,
-  // examples: {
-  //   part1: {
-  //     input: `example input here`,
-  //     expected: 123
-  //   },
-  //   part2: {
-  //     input: `example input here`,
-  //     expected: 456
-  //   }
-  // }
-}
+  examples: {
+    part1: {
+      input: `L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L821`,
+      expected: 123
+    }
+    // part2: {
+    //   input: `example input here`,
+    //   expected: 456
+    // }
+  }
+};
