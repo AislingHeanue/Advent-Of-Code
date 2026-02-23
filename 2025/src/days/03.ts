60d365791a684e96db267eb154a5a1b48106fa01
AislingHeanue <aislingheanue@gmail.com>
2026-02-23 23:32:38 +0000

    Complete Day 3 and 4

--- 2025/src/days/03.ts
+++ 2025/src/days/03.ts
@@ -1,23 +1,45 @@
+import { Console, Effect, Stream } from "effect";
 import type { Input } from "../input";
 
 export function* solve(input: Input, part: Part) {
-  if (part === 1) {
-    return undefined;
-  } else {
-    return undefined;
-  }
+  const nums = part === 1 ? 2 : 12;
+  return yield* input.stream.pipe(
+    Stream.map(toNumberArray),
+    Stream.mapEffect(a => getHighest(a, nums)),
+    // Stream.tap(Console.log),
+    Stream.runSum
+  );
 }
 
+const toNumberArray = (line: string) => line.split("").map(a => +a);
+
+const getHighest = (line: number[], nums: number) =>
+  Effect.gen(function* () {
+    const out = new Array(nums);
+    let index = 0;
+    for (let i = 1; i <= nums; i++) {
+      out[i - 1] = Math.max(...line.slice(index, -nums + i || undefined));
+      index += line.slice(index).indexOf(out[i - 1]) + 1;
+    }
+    return +out.join("");
+  });
+
 export const solution: DaySolution = {
-  solve
-  // examples: {
-  //   part1: {
-  //     input: `example input here`,
-  //     expected: 123
-  //   },
-  //   part2: {
-  //     input: `example input here`,
-  //     expected: 456
-  //   }
-  // }
+  solve,
+  examples: {
+    part1: {
+      input: `987654321111111
+811111111111119
+234234234234278
+818181911112111`,
+      expected: 357
+    },
+    part2: {
+      input: `987654321111111
+811111111111119
+234234234234278
+818181911112111`,
+      expected: 3121910778619
+    }
+  }
 };
--- 2025/src/days/04.ts
+++ 2025/src/days/04.ts
@@ -1,23 +1,120 @@
+import { Console, Effect, Option, Stream } from "effect";
 import type { Input } from "../input";
 
+const printing = false;
+
 export function* solve(input: Input, part: Part) {
-  if (part === 1) {
-    return undefined;
-  } else {
-    return undefined;
-  }
+  const grid = input.grid.map(line => line.map(c => c === "@"));
+
+  let pipeline = Stream.range(0, grid[0]!.length - 1).pipe(
+    Stream.zipWithPreviousAndNext,
+    Stream.cross(
+      Stream.range(0, grid.length - 1).pipe(Stream.zipWithPreviousAndNext)
+    ),
+    // Stream.tap(a => (a[1][1] === 0 ? print("\n") : Effect.void)),
+    // Stream.tap(a =>
+    //   printing && Option.isSome(canAccess(a, grid))
+    //     ? print("x")
+    //     : print(grid[a[0][1]]![a[1][1]]! ? "@" : ".")
+    // ),
+    Stream.filterMap(a => canAccess(a, grid)),
+    Stream.runCollect,
+    Effect.map(chunk => {
+      for (const a of chunk) {
+        modifyGrid(a, grid);
+      }
+
+      return chunk.length;
+    })
+  );
+
+  const res = Effect.iterate(
+    { cont: true, returned: 0 },
+    {
+      while: ({ cont }) => cont,
+      body: ({ returned }) =>
+        pipeline.pipe(
+          Effect.map(a => ({
+            cont: a > 0 && part !== 1,
+            returned: a + returned
+          }))
+        )
+    }
+  );
+
+  const returned = (yield* res).returned;
+  yield* print("\n");
+  return returned;
 }
 
+const print = (message: string) =>
+  Effect.sync(() => {
+    if (printing) {
+      process.stdout.write(message);
+    }
+  });
+
+const canAccess = (
+  input: [
+    [Option.Option<number>, number, Option.Option<number>],
+    [Option.Option<number>, number, Option.Option<number>]
+  ],
+  grid: boolean[][]
+): Option.Option<[number, number]> => {
+  if (!grid[input[0][1]]![input[1][1]]!) {
+    return Option.none();
+  }
+  let count = 0;
+  for (let xi = 0; xi <= 2; xi++) {
+    for (let yi = 0; yi <= 2; yi++) {
+      let y = input[0][yi];
+      let x = input[1][xi];
+      if (Option.isOption(y)) {
+        y = Option.getOrUndefined(y);
+      }
+      if (Option.isOption(x)) {
+        x = Option.getOrUndefined(x);
+      }
+      if (x !== undefined && y !== undefined) {
+        count += +grid[y]![x]!;
+      }
+    }
+  }
+  return count <= 4 ? Option.some([input[0][1], input[1][1]]) : Option.none();
+};
+
+const modifyGrid = (input: [number, number], grid: boolean[][]) => {
+  grid[input[0]]![input[1]] = false;
+};
+
 export const solution: DaySolution = {
-  solve
-  // examples: {
-  //   part1: {
-  //     input: `example input here`,
-  //     expected: 123
-  //   },
-  //   part2: {
-  //     input: `example input here`,
-  //     expected: 456
-  //   }
-  // }
+  solve,
+  examples: {
+    part1: {
+      input: `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`,
+      expected: 13
+    },
+    part2: {
+      input: `..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.`,
+      expected: 43
+    }
+  }
 };
